10ba402352d2bce79352d8e769672562b75522c8
Igor Tukh <igor-tukh@yandex.ru>
2019-03-08 18:28:22 +0300

    model: field unpickle option removed

--- src/model/field.py
+++ src/model/field.py
@@ -1,14 +1,11 @@
 class Field(object):
-    def __init__(self, height, width, field_file=None):
-        if field_file is None:
-            self.height = height
-            self.width = width
-            self.rooms = {}
-            self.doors = {}
-            self.game_objects = {}
-            self._generate_content()
-        else:
-            pass
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.rooms = {}
+        self.doors = {}
+        self.game_objects = {}
+        self._generate_content()
 
     def _generate_content(self):
         pass
