781171b98acc3b4cb2436c1bc162d5f3b82b84d9
andrey <andrey.smirdin@gmail.com>
2019-03-08 20:10:35 +0300

    parsing arguments

--- start.py
+++ start.py
@@ -1,5 +1,14 @@
+import sys
+
 from src.controller.controller import Controller
 
 if __name__ == "__main__":
-    controller = Controller()
+    if len(sys.argv) == 2:
+        controller = Controller(sys.argv[1])
+    elif len(sys.argv) == 1:
+        controller = Controller()
+    else:
+        print("Incorrect usage. Please provide only 1 optional argument with pickle field.")
+        exit(0)
+
     controller.start()
