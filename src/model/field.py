98695741075f4ff8117a22ac7331867afba68e42
Igor Tukh <igor-tukh@yandex.ru>
2019-03-08 18:20:34 +0300

    model: deserialize option arg added

--- src/model/field.py
+++ src/model/field.py
@@ -1,7 +1,14 @@
 class Field(object):
-    def __init__(self, height, width):
-        self.height = height
-        self.width = width
-        self.rooms = {}
-        self.doors = {}
-        self.game_objects = {}
+    def __init__(self, height, width, field_file=None):
+        if field_file is None:
+            self.height = height
+            self.width = width
+            self.rooms = {}
+            self.doors = {}
+            self.game_objects = {}
+            self._generate_content()
+        else:
+            pass
+
+    def _generate_content(self):
+        pass
