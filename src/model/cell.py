713fd497fffffc7877f9c7fcf12716071ab25ded
Igor Tukh <igor-tukh@yandex.ru>
2019-03-08 20:27:29 +0300

    model: after fucking refactor

--- src/model/cell.py
+++ src/model/cell.py
@@ -1,5 +1,14 @@
+from enum import Enum
+
+
+class CellType(Enum):
+    WALL = 1
+    FLOOR = 2
+
+
 class Cell(object):
-    def __init__(self, row, column, parent):
+    def __init__(self, row, column, cell_type, is_visible):
         self.row = row
         self.column = column
-        self._parent = parent
+        self.cell_type = cell_type
+        self.is_visible = is_visible
