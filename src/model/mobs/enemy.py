a397e666918fd87c63f7ca6bc1d0034fec75390d
Aleksandr Kirakosian <kair1998@gmail.com>
2019-04-16 20:35:41 +0300

    Fixed init

--- src/model/mobs/enemy.py
+++ src/model/mobs/enemy.py
@@ -2,16 +2,14 @@ from src.model.game_object import GameObject
 
 
 class Enemy(GameObject):
-    def __init__(self):
-        self.health = 3
-        self.base_damage = 2
-
     """
     Enemy 'context' class. Stores certain mob fighting strategy.
     """
     def __init__(self, cell):
         super(Enemy, self).__init__(cell)
         self.fighting_strategy = None
+        self.health = 3
+        self.base_damage = 2
 
     def set_fighting_strategy(self, strategy):
         self.fighting_strategy = strategy
