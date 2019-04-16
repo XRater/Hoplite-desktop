18b0b853445b06884250e00cccdd4344439394ec
Igor Tukh <igor-tukh@yandex.ru>
2019-04-16 20:06:39 +0300

    enemies-and-fighting: enemy turn and fighting strategy signature changed

--- src/model/mobs/fighting_strategy.py
+++ src/model/mobs/fighting_strategy.py
@@ -3,4 +3,10 @@ class FightingStrategy(object):
     Fighting strategy
     """
     def attack_player(self, field, current_cell):
+        """
+        Describes enemy turn according to the certain strategy
+        :param field: dungeon field
+        :param current_cell: current enemy position
+        :return: list of EnemyTurn objects
+        """
         raise NotImplementedError()
