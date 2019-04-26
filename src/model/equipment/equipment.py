be1a93696dbc385460187b1786071f89fb02769c
Igor Tukh <igor-tukh@yandex.ru>
2019-04-26 20:50:50 +0300

    equipment-loading-interaction: equimpment description added

--- src/model/equipment/equipment.py
+++ src/model/equipment/equipment.py
@@ -9,8 +9,9 @@ class Equipment(GameObject):
         SHOES = 3
         WEAPON = 4
 
-    def __init__(self, cell, equipment_type, damage_boost, damage_absorption):
+    def __init__(self, cell, description, equipment_type, damage_boost, damage_absorption):
         super(Equipment, self).__init__(cell)
         self.equipment_type = equipment_type
         self.damage_boost = damage_boost
         self.damage_absorption = damage_absorption
+        self.description = description
