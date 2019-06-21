13255149eaac06e50dd5779eae3fd75f57fd8a33
Aleksandr Kirakosian <kair1998@gmail.com>
2019-06-21 19:39:46 +0300

    Now strategy is decorated

---
+++ src/model/mobs/enemy/fighting_strategy/confused_decorator.py
@@ -0,0 +1,19 @@
+from src.model.mobs.enemy.enemy_turn import EnemyTurn
+from random import choice
+
+
+class ConfusedStrategyDecorator:
+
+    def __init__(self, strategy, confusion_duration=5):
+        self._strategy = strategy
+        self.confusion_duration = confusion_duration
+
+    def create_turn(self, field, current_cell):
+        if self.confusion_duration == 0:
+            return self._strategy.create_turn(field, current_cell)
+        self.confusion_duration -= 1
+        moves = [move.value for move in EnemyTurn]
+        return [choice(moves)]
+
+    def unwrap(self):
+        return self._strategy
